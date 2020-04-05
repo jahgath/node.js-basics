e15c24e5ec4c3d3b40fa702eaa72cfc8aa08d124
jahgath <jagathjijo01@gmail.com>
2020-04-05 23:47:05 +0530

    readable stream

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+var http = require('http');
 var fs = require('fs');
+//
+// var server = http.createServer(function(req,res) {
+//   console.log('request was made '+req.url);
+//   res.writeHead(200,{'Content-type': 'text/plain'});
+//   res.end('Hey ninjas');
+// });
+//
+// server.listen(3000,'127.0.0.1');
+// console.log('yo dawgs, now listening to port 3000');
+//
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
 
-fs.readFile('readme.txt', 'utf8' ,function(err,data) {
-  console.log(data);
-  fs.writeFile('writeme.txt',data,function() {
-
-  });
-});
+myReadStream.on('data',function(chunk) {
+  console.log('new chunk recieved:');
+  console.log(chunk);
+})
